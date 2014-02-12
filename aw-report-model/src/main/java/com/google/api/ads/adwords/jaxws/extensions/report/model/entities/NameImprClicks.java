2643d916cb6836e8dacbccda0e61e21234669917
Mark R. Bowyer <markbowyer@google.com>
2014-02-12 18:35:27 -0500

    Adding in summations for AdExtensions reports, Part 2

---
+++ aw-report-model/src/main/java/com/google/api/ads/adwords/jaxws/extensions/report/model/entities/NameImprClicks.java
@@ -0,0 +1,8 @@
+package com.google.api.ads.adwords.jaxws.extensions.report.model.entities;
+
+public class NameImprClicks {
+  public String clickType;
+  public Long impressions = 0L;
+  public Long clicks = 0L;
+}
+
