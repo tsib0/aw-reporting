073c4193c332f74a918c7da0055f06b41828701d
Julian Toledo <juliantoledo@gmail.com>
2014-03-03 16:26:01 +0100

    Adding new structure for UnitTests

---
+++ examples/kratubackend/src/test/java/com/google/api/ads/adwords/jaxws/extensions/kratu/data/KratuTest.java
@@ -0,0 +1,32 @@
+//Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//       http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.api.ads.adwords.jaxws.extensions.kratu.data;
+
+/**
+ * Test case for the {@code Kratu} class.
+ *
+ * @author
+ */
+public class KratuTest {
+
+  public void test_createKratuSummary() {
+  }
+
+  /*
+   * Retrieves 7 reports types per account and creates a Kratu data object for each day.
+   */
+  public void test_createDailyKratuFromDB() {
+  }
+}
