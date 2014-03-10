a438b6172b3909729ce3228f685b253896b69169
Julian Toledo <juliantoledo@gmail.com>
2014-03-10 16:04:32 +0100

    Fixes for PDF/HTML Exporters

--- aw-reporting/src/main/java/com/google/api/ads/adwords/jaxws/extensions/exporter/reportwriter/ReportWriterType.java
+++ aw-reporting/src/main/java/com/google/api/ads/adwords/jaxws/extensions/exporter/reportwriter/ReportWriterType.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.api.ads.adwords.jaxws.extensions.reportwriter;
+package com.google.api.ads.adwords.jaxws.extensions.exporter.reportwriter;
 
 /**
  * Enum to describe the type of {@link ReportWriter} used.
