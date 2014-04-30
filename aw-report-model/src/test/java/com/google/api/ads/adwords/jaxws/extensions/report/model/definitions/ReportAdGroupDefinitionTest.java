3a593b61f1bb4fbae1b42cbf6fa7f9bd6a6a288e
Julian Toledo <juliantoledo@gmail.com>
2014-04-30 04:55:56 -0700

    Fixes for Issue #27

--- aw-report-model/src/test/java/com/google/api/ads/adwords/jaxws/extensions/report/model/definitions/ReportAdGroupDefinitionTest.java
+++ aw-report-model/src/test/java/com/google/api/ads/adwords/jaxws/extensions/report/model/definitions/ReportAdGroupDefinitionTest.java
@@ -48,14 +48,14 @@ public class ReportAdGroupDefinitionTest extends AbstractReportDefinitionTest<Re
 
     Assert.assertEquals(2450945640L, first.getAccountId().longValue());
     Assert.assertEquals("2013-05-07", first.getDay());
-    Assert.assertEquals("2.72", first.getCost());
+    Assert.assertEquals(2.72, first.getCostBigDecimal().doubleValue());
     Assert.assertEquals(6L, first.getClicks().longValue());
     Assert.assertEquals(16L, first.getImpressions().longValue());
     Assert.assertEquals(0L, first.getConversions().longValue());
-    Assert.assertEquals("37.50", first.getCtr());
-    Assert.assertEquals("170.00", first.getAvgCpm());
-    Assert.assertEquals("0.45", first.getAvgCpc());
-    Assert.assertEquals("2.69", first.getAvgPosition());
+    Assert.assertEquals(37.50, first.getCtrBigDecimal().doubleValue());
+    Assert.assertEquals(170.00, first.getAvgCpmBigDecimal().doubleValue());
+    Assert.assertEquals(0.45, first.getAvgCpcBigDecimal().doubleValue());
+    Assert.assertEquals(2.69, first.getAvgPositionBigDecimal().doubleValue());
     Assert.assertEquals("EUR", first.getCurrencyCode());
     Assert.assertEquals(129807304L, first.getCampaignId().longValue());
     Assert.assertEquals(7253055064L, first.getAdGroupId().longValue());
@@ -73,14 +73,14 @@ public class ReportAdGroupDefinitionTest extends AbstractReportDefinitionTest<Re
 
     Assert.assertEquals(2450945640L, last.getAccountId().longValue());
     Assert.assertEquals("2013-05-10", last.getDay());
-    Assert.assertEquals("0.60", last.getCost());
+    Assert.assertEquals(0.60, last.getCostBigDecimal().doubleValue());
     Assert.assertEquals(1L, last.getClicks().longValue());
     Assert.assertEquals(72L, last.getImpressions().longValue());
     Assert.assertEquals(0L, last.getConversions().longValue());
-    Assert.assertEquals("1.39", last.getCtr());
-    Assert.assertEquals("8.33", last.getAvgCpm());
-    Assert.assertEquals("0.60", last.getAvgCpc());
-    Assert.assertEquals("2.58", last.getAvgPosition());
+    Assert.assertEquals(1.39, last.getCtrBigDecimal().doubleValue());
+    Assert.assertEquals(8.33, last.getAvgCpmBigDecimal().doubleValue());
+    Assert.assertEquals(0.60, last.getAvgCpcBigDecimal().doubleValue());
+    Assert.assertEquals(2.58, last.getAvgPositionBigDecimal().doubleValue());
     Assert.assertEquals("EUR", last.getCurrencyCode());
     Assert.assertEquals(129807304L, last.getCampaignId().longValue());
     Assert.assertEquals(7253055064L, last.getAdGroupId().longValue());
