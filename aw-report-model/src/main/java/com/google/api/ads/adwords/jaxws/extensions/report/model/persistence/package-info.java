855746ca14c1792f252c55acd4147301ee7aff2b
Gustavo Moreira <gustavomoreira@google.com>
2014-04-30 11:47:15 -0300

    Adding Search Query performance report

--- aw-report-model/src/main/java/com/google/api/ads/adwords/jaxws/extensions/report/model/persistence/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * Persistence layer package.
- *
- * Includes all the interfaces and implementations supported by the model.
- */
-package com.google.api.ads.adwords.jaxws.extensions.report.model.persistence;
