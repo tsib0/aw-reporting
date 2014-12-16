e46835775c83721f65697c78d8f928e40bc7cac1
Joel Toby <joeltoby@gmail.com>
2014-12-16 11:09:26 +0000

    Refactoring

---
+++ aw-reporting-server/src/html/awrc/app/controllers/settingsdropdowncontroller.js
@@ -0,0 +1,15 @@
+
+awrcApp.controller("settingsdropdowncontroller", function ($scope, $log) {
+	$scope.status = {
+			isopen: false
+	};
+
+	$scope.toggled = function(open) {
+	};
+
+	$scope.toggleDropdown = function($event) {
+		$event.preventDefault();
+		$event.stopPropagation();
+		$scope.status.isopen = !$scope.status.isopen;
+	};
+});
