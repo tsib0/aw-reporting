079275b79ebff56e4e3660a693994e5d19549816
Chen Zhuo <chenzhuo914@gmail.com>
2015-04-29 15:06:42 +0800

    Remove duplicate "AdGroupName" field from merge

--- aw-reporting-model/src/test/java/com/google/api/ads/adwords/awreporting/model/definitions/ReportKeywordDefinitionTest.java
+++ aw-reporting-model/src/test/java/com/google/api/ads/adwords/awreporting/model/definitions/ReportKeywordDefinitionTest.java
@@ -168,7 +168,6 @@ public class ReportKeywordDefinitionTest extends AbstractReportDefinitionTest<Re
         // Specific to Keyword Performance Report
         "CampaignId",
         "AdGroupId",
-        "AdGroupName",
         "Id",
         "Status",
         "QualityScore",
